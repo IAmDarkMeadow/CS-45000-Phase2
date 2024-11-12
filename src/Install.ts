dede2305de7493795e8048185d2c40250afab463
gdeherdt <gdeherdt@purdue.edu>
2024-11-12 17:13:07 -0500

    S3 download command and test file for formatting

--- src/Install.ts
+++ src/Install.ts
@@ -23,5 +23,6 @@ function installPak(packageName:string){
 
 installPak('dotenv');
 installPak('winston');
+installPak('node-ssh');
 
 process.exit(0);
---
+++ src/test.ts
@@ -0,0 +1,16 @@
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import {fromEnv} from '@aws-sdk/credential-provider-env'
+import {downloadFileS3} from './Download.js'
+import {AwsCredentialIdentity} from '@smithy/types'
+
+//fromEnv()
+let serverLink = 'ec2-3-144-182-107.us-east-2.compute.amazonaws.com';
+let s3client = new S3Client({
+    region:'us-east-2',
+    endpoint:serverLink,
+    credentials:{
+        accessKeyId:'',
+        secretAccessKey:''
+    }
+});
+downloadFileS3(s3client,'registry-storage/Modules/', 'test.txt','./');
