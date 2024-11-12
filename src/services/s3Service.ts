4b0595a538a2190bd30e14fd74fcf8d617e1352b
bdevenpo741 <bdevenpo@purdue.edu>
2024-11-12 16:10:36 -0500

    Update s3Service.ts

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -1,12 +1,21 @@
+//s3Sercise.ts
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const access_key = process.env.Your_access_key;
+const secret_access_key = process.env.Your_Secret_access_key;
 
 const s3 = new AWS.S3({
-  region: 'your-region', // e.g., 'us-east-1'
-  accessKeyId: 'your-access-key',
-  secretAccessKey: 'your-secret-key',
+  region: 'us-east-2', // e.g., 'us-east-1'
+  accessKeyId: access_key,
+  secretAccessKey: secret_access_key,
 });
 
-const BUCKET_NAME = 'your-bucket-name';
+//Updated with BUCKET_Name
+
+const BUCKET_NAME = 'registry-storage';
 
 export const uploadToS3 = async (fileBuffer: Buffer, key: string) => {
   const params = {
